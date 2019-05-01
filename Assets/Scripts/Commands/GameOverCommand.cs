f145e36b021d8ee7d08609319a086cae252f84ac
nico0926 <38829437+nico0926@users.noreply.github.com>
2019-05-01 14:08:33 -0500

    Analytics Added
    Analíticas añadidas para cuando se abra la tienda, para saber cual opción de compra se usa más, y el número de game overs

--- Assets/Scripts/Commands/GameOverCommand.cs
+++ Assets/Scripts/Commands/GameOverCommand.cs
@@ -1,17 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
 
 public class GameOverCommand : Command{
 
     private Player looser;
+    public int gameOverCount;
 
     public GameOverCommand(Player looser)
     {
         this.looser = looser;
+        gameOverCount++;
+        Analytics.CustomEvent("Deads", new Dictionary<string, object> {
+            {"NumberofGameovers",gameOverCount}
+
+             });
+        
     }
 
     public override void StartCommandExecution()
     {
         looser.PArea.Portrait.Explode();
     }
+
+    
+
+
+
 }
