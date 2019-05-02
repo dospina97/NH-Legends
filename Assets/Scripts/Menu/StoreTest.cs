af1958b277928e34fee20f3f124c5440d1beb1cd
dospina97 <47892916+dospina97@users.noreply.github.com>
2019-05-02 00:02:15 -0500

    Store Test
    Store Test (tienda simulada, tomó 10 minutos era bien fácil, hacerle persistencia se demoraba máximo una hora pero no pude porque estaba solucionando un problema con el keystore que no dejaba tirar APK.

---
+++ Assets/Scripts/Menu/StoreTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreTest : MonoBehaviour
+{
+    
+    public Text Creditos;
+
+    
+    public int CreditosInt = 550;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Creditos.text = CreditosInt.ToString(); 
+
+    }
+
+    public void BuyTest5()
+    {
+        CreditosInt -= 5;
+        Creditos.text = CreditosInt.ToString();
+
+    }
+
+    public void BuyTest15()
+    {
+
+        CreditosInt -= 15;
+        Creditos.text = CreditosInt.ToString();
+
+    }
+
+    public void BuyTest40()
+    {
+        CreditosInt -= 40;
+        Creditos.text = CreditosInt.ToString();
+
+
+    }
+}
